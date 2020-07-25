6aa57a93edbb1ac291bcd5f444af7654cb822568
ayushgarg0694 <ayushgarg0694@gmail.com>
2020-07-25 00:57:46 -0400

    initial commit

---
+++ smartcab/BasicDrivingAgent.py
@@ -0,0 +1,94 @@
+import random
+import math
+from environment import Agent, Environment
+from planner import RoutePlanner
+from simulator import Simulator
+
+class LearningAgent(Agent):
+    """ An agent that learns to drive in the Smartcab world.
+        This is the object you will be modifying. """ 
+
+    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5):
+        super(LearningAgent, self).__init__(env)     # Set the agent in the evironment 
+        self.planner = RoutePlanner(self.env, self)  # Create a route planner
+        
+        
+        self.valid_actions = self.env.valid_actions  # The set of valid actions
+
+        # Set parameters of the learning agent
+        self.learning = learning # Whether the agent is expected to learn
+        self.Q = dict()          # Create a Q-table which will be a dictionary of tuples
+        self.epsilon = epsilon   # Random exploration factor
+        self.alpha = alpha       # Learning factor
+
+    def reset(self, destination=None, testing=False):
+        """ The reset function is called at the beginning of each trial.
+            'testing' is set to True if testing trials are being used
+            once training trials have completed. """
+
+        # Select the destination as the new location to route to
+        self.planner.route_to(destination)
+        return None
+
+    def update(self):
+        """ The update function is called when a time step is completed in the 
+            environment for a given trial. This function will build the agent
+            state, choose an action, receive a reward, and learn if enabled. """
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+
+        # TODO: Select action according to your policy
+        action = random.choice(Environment.valid_actions)
+
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        return
+        
+
+def run():
+    """ Driving function for running the simulation. 
+        Press ESC to close the simulation, or [SPACE] to pause the simulation. """
+
+    ##############
+    # Create the environment
+    # Flags:
+    #   verbose     - set to True to display additional output from the simulation
+    #   num_dummies - discrete number of dummy agents in the environment, default is 100
+    #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
+    env = Environment()
+    
+    ##############
+    # Create the driving agent
+    # Flags:
+    #   learning   - set to True to force the driving agent to use Q-learning
+    #    * epsilon - continuous value for the exploration factor, default is 1
+    #    * alpha   - continuous value for the learning rate, default is 0.5
+    agent = env.create_agent(LearningAgent)
+    
+    ##############
+    # Follow the driving agent
+    # Flags:
+    #   enforce_deadline - set to True to enforce a deadline metric
+    env.set_primary_agent(agent,enforce_deadline=False)
+
+    ##############
+    # Create the simulation
+    # Flags:
+    #   update_delay - continuous time (in seconds) between actions, default is 2.0 seconds
+    #   display      - set to False to disable the GUI if PyGame is enabled
+    #   log_metrics  - set to True to log trial and simulation results to /logs
+    #   optimized    - set to True to change the default log file name
+    sim = Simulator(env,update_delay = 1.0)
+    
+    ##############
+    # Run the simulator
+    # Flags:
+    #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
+    #   n_test     - discrete number of testing trials to perform, default is 0
+    sim.run(n_test = 1)
+
+
+if __name__ == '__main__':
+    run()
